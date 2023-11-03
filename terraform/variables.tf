a336c306502dcfd215e8eb91da4c56352f981d1c
Diego Brito <dibbrito@gmail.com>
2023-11-02 22:51:26 -0300

    fix variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,9 @@
 variable "db_user" {
   description = "GitHub secret for database user"
-  default     = var.db_user != "" ? var.db_user : ""
+  default     =  ""
 }
 
 variable "db_password" {
   description = "GitHub secret for database password"
-  default     = var.db_password != "" ? var.db_password : ""
+  default     = ""
 }
