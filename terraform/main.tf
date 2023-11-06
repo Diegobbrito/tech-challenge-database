5f7d0c1bc30979acd655ca214fd4d49d03cfcad0
Diego Brito <dibbrito@gmail.com>
2023-11-05 22:17:32 -0300

    fix mysql version

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,25 +3,31 @@ provider "aws" {
 }
 
 resource "aws_db_instance" "lanchonetedb" {
-  identifier           = "dblanchonete"
+  identifier           = "lanchonetedb"
   instance_class       = "db.t2.micro"
   engine               = "mysql"
   allocated_storage    = 20
+  storage_type         = "gp2"
+  db_name              = "lanchonete"
   username             = var.db_user
   password             = var.db_password
   db_subnet_group_name = aws_db_subnet_group.lanchonetedb.name
-  parameter_group_name = "default.mysql5.7"
+  parameter_group_name = "default.mysql8.0"
 }
 
 resource "aws_db_subnet_group" "lanchonetedb" {
   name       = "db-subnet-group"
-  subnet_ids = aws_subnet.lanchonetedb.*.id
+  subnet_ids = [
+    aws_subnet.lanchonetedb[0].id,
+    aws_subnet.lanchonetedb[1].id
+  ]
 }
+
 
 resource "aws_subnet" "lanchonetedb" {
   count                   = 2
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
+  cidr_block              = "10.0.${count.index + 1}.0/24"
+  availability_zone       = element(["us-east-1a", "us-east-1b"], count.index)
   vpc_id                  = aws_vpc.lanchonetedb.id
   map_public_ip_on_launch = true
 }
