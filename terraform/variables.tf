c65a06e1ac57199d5fd1732eeda82048583f69d7
Diego Brito <dibbrito@gmail.com>
2023-11-08 21:36:35 -0300

    update files

--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "db_user" {
-  description = "GitHub secret for database user"
-  default     =  ""
-}
-
-variable "db_password" {
-  description = "GitHub secret for database password"
-  default     = ""
-}
