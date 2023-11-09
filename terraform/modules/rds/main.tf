bbf9c185e31da60c67ab1db26060b9008999cb2c
Diego Brito <dibbrito@gmail.com>
2023-11-08 21:39:44 -0300

    add rds module with workflow

---
+++ terraform/modules/rds/main.tf
@@ -0,0 +1,65 @@
+provider "aws" {
+  region     = "${var.region}"
+}
+
+resource "aws_db_subnet_group" "default" {
+  name        = "main_subnet_group"
+  description = "Our main group of subnets"
+  subnet_ids  = ["${aws_subnet.subnet_1.id}", "${aws_subnet.subnet_2.id}"]
+}
+resource "aws_subnet" "subnet_1" {
+  vpc_id            = "${var.vpc_id}"
+  cidr_block        = "${var.subnet_1_cidr}"
+  availability_zone = "${var.az_1}"
+
+  tags = {
+    Name = "main_subnet1"
+  }
+}
+
+resource "aws_subnet" "subnet_2" {
+  vpc_id            = "${var.vpc_id}"
+  cidr_block        = "${var.subnet_2_cidr}"
+  availability_zone = "${var.az_2}"
+
+  tags = {
+    Name = "main_subnet2"
+  }
+}
+resource "aws_security_group" "default" {
+  name        = "main_rds_sg"
+  description = "Allow all inbound traffic"
+  vpc_id      = "${var.vpc_id}"
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "TCP"
+    cidr_blocks = ["${var.cidr_blocks}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.sg_name}"
+  }
+}
+
+resource "aws_db_instance" "lanchonete" {
+  identifier             = "${var.identifier}"
+  allocated_storage      = "${var.storage}"
+  engine                 = "${var.engine}"
+  engine_version         = "${lookup(var.engine_version, var.engine)}"
+  instance_class         = "${var.instance_class}"
+  db_name                   = "${var.db_name}"
+  username               = "${var.username}"
+  password               = "${var.password}"
+  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+  db_subnet_group_name   = "${aws_db_subnet_group.default.id}"
+  skip_final_snapshot = "true"
+}
