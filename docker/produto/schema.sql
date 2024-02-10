d7ce40526eb279d72752542a5f4f245b4cba914f
Diego Brito <dibbrito@gmail.com>
2024-02-09 22:11:44 -0300

    adc arquivos para cada banco de dados de microserviços

--- docker/produto/schema.sql
+++ docker/produto/schema.sql
@@ -1,14 +1,5 @@
 CREATE DATABASE IF NOT EXISTS lanchonete;
 USE lanchonete;
-CREATE TABLE clientes (
-    id INT NOT NULL AUTO_INCREMENT,
-    cpf VARCHAR(20),
-    nome VARCHAR(200),
-    email VARCHAR(200),
-    PRIMARY KEY (id),
-    UNIQUE KEY (cpf),
-    UNIQUE KEY (email)
-);
 
 CREATE TABLE produtos (
     id INT NOT NULL AUTO_INCREMENT,
@@ -26,48 +17,6 @@ CREATE TABLE categorias (
     descricao VARCHAR(200),
     PRIMARY KEY (id)
 );
-
-CREATE TABLE status_pedido (
-    id INT NOT NULL AUTO_INCREMENT,
-    titulo VARCHAR(200) NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE usuarios (
-    id INT NOT NULL AUTO_INCREMENT,
-    nome VARCHAR(200),
-    senha VARCHAR(200),
-    permissoes VARCHAR(200),
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE pedidos (
-    id INT NOT NULL AUTO_INCREMENT,
-    data_criacao DATE,
-    valor DECIMAL(10,2) NOT NULL,
-    is_cliente BOOLEAN NOT NULL,
-    cliente_id INT,
-    status_id INT,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE pedido_produto (
-    id INT NOT NULL AUTO_INCREMENT,
-    pedido_id INT NOT NULL,
-    produto_id INT NOT NULL,
-    quantidade INT NOT NULL,
-    FOREIGN KEY (pedido_id) REFERENCES pedidos(id),
-    FOREIGN KEY (produto_id) REFERENCES produtos(id),
-    PRIMARY KEY (id)
-);
-
-SET character_set_client = utf8;
-SET character_set_connection = utf8;
-SET character_set_results = utf8;
-SET collation_connection = utf8_general_ci;
-
-INSERT INTO usuarios (nome, senha, permissoes) VALUES ("usuario", "$2y$10$apny7Z7eYoQP4MnGzgciEerREBrgAVl1rtD3OoEywjd2XLiHniMaO", "USER");
-INSERT INTO usuarios (nome, senha, permissoes) VALUES ("admin", "$2y$10$AymKRLbdwvPArl8IOIq1k.gmsbN3ev7wGtrx4kpDo1f7Uz2/sSNeS", "ADMIN,USER");
 
 INSERT INTO categorias (titulo, descricao) VALUES ("Lanches", "Lanches são compostos por pão, hambúrguer e outros ingredientes como alface, tomate, queijo, bacon, entre outros");
 INSERT INTO categorias (titulo, descricao) VALUES ("Bebidas", "Bebidas como água, refrigerantes, sucos e cervejas");
@@ -96,12 +45,6 @@ INSERT INTO produtos (nome, descricao, valor, imagem_url, categoria_id) VALUES (
 INSERT INTO produtos (nome, descricao, valor, imagem_url, categoria_id) VALUES ("Brownie de Nutella e Ninho", "Delicioso brownie coberto com ganache de Nutella e leite Ninho em pó.", 9.90, "https://static.saipos.com/saipos-estatico/img-items/17862/store_item/e57fd6908610d97f0156b3676d67164e.jpeg?d=x250", 4);
 #Combo
 INSERT INTO produtos (nome, descricao, valor, imagem_url, categoria_id) VALUES ("Combo MELT + Coca + Fritas", "Pão australiano, hambúrguer Angus de 150g, queijo cheddar cremoso, cebola caramelizada, bacon super crocante e molho barbecue. + Coca Cola Lt 350ml + Porção de fritas crocantes.(120g)", 39.90, "https://static.ifood-static.com.br/image/upload/t_medium/pratos/3c25c30b-c376-4ccb-99a4-4df645726a84/202208241937_IEC0_i.jpg", 5);
-#Status
-INSERT INTO status_pedido (titulo) VALUES ("Pagamento Pendente");
-INSERT INTO status_pedido (titulo) VALUES ("Recebido");
-INSERT INTO status_pedido (titulo) VALUES ("Em preparação");
-INSERT INTO status_pedido (titulo) VALUES ("Pronto");
-INSERT INTO status_pedido (titulo) VALUES ("Finalizado");
 
-GRANT ALL PRIVILEGES ON *.* TO 'usuario'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'pedido'@'%' WITH GRANT OPTION;
 flush privileges;
